cd942a75ade7b48fe3ace220fafc77ab46455918
Khoiram Sohan Singh <kssingh@gainsight.com>
2021-09-23 01:31:22 +0530

    Doing CRUD ops in Spring JDBC

--- database/src/main/java/com/example/database/DatabaseApplication.java
+++ database/src/main/java/com/example/database/DatabaseApplication.java
@@ -1,12 +1,15 @@
 package com.example.database;
 
 import com.example.database.jdbc.dao.PersonDAO;
+import com.example.database.jdbc.models.Person;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Date;
 
 
 @SpringBootApplication
@@ -23,5 +26,8 @@ public class DatabaseApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		logger.info("Users in db: {}", personDao.findAll());
+		logger.info("User Sohan in db : {}", personDao.findByName("Sohan"));
+		logger.info("Inserting 10004 -> {}", personDao.insert(new Person(10004, "Shanit", "Shillong", new Date())));
+		logger.info("Update 10003 -> {}", personDao.update(new Person(10003, "Roger", "Guwahati", new Date())));
 	}
 }
